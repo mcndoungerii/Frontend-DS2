62628134be3b6b94d07aa9a0d5ead06be38db00d
Allen Mwandunga <allenmcndounger@gmail.com>
2024-06-26 22:53:56 +0200

    update tests script

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Frontend-DS2' title`, () => {
+  it(`should have the 'Hello World' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Frontend-DS2');
+    expect(app.title).toEqual('Hello World');
   });
 
   it('should render title', () => {
